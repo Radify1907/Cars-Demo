c2f64edb2e1380c7c21e3f88dcdbdbcee2c3e2de
Radify1907 <ebekhit@gmail.com>
2017-06-21 22:38:39 +1000

    Add files via upload
    Test files for cars project.

---
+++ Cars/Cars/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports ClassLibrary1
+
+Module Module1
+
+    Sub Main()
+        Dim myCar As New Cars
+        myCar.make = make.Honda
+        myCar.model = "MDX"
+        Dim myOtherCar As New Cars
+        myOtherCar.make = make.MercedesBenz
+        myOtherCar.model = "Viano"
+
+        Dim CC As New System.Collections.ArrayList
+        CC.Add(myCar)
+        CC.Add(myOtherCar)
+
+        'CC.Sort()
+
+        For Each cars In CC
+            Dim i As Integer
+
+            Console.WriteLine("We own a {0} {1}.", cars.make, cars.model)
+
+
+
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
